26c1e9a6255e4b9e173577e5a26b34a9acde4cfa
Ambroise Piganeau <ambroise.piganeau@gmail.com>
2015-10-07 13:20:28 -0700

    queues tests all pass

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,15 +6,22 @@ var Queue = function() {
 
   // Implement the methods below
   var length = 0;
+  var startIndex = 0;
 
   someInstance.enqueue = function(value) {
+    storage[startIndex+length] = value;
     length++;
   };
 
   someInstance.dequeue = function() {
+    var element;
     if (length > 0) {
+      element = storage[startIndex];
+      delete storage[startIndex];
+      startIndex++;
       length--;
     }
+    return element;
   };
 
   someInstance.size = function() {
