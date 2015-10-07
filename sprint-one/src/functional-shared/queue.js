4d27b2596e984e0fb7ac93e9a2771cc859736645
apiga <ambroise.piganeau@gmail.com>
2015-10-07 14:58:28 -0700

    functionnal-shared pattern for queues completed

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newestIndex = 1;
+  var oldestIndex = 1;
+  var content = {};
+  var queue = {
+    newestIndex: newestIndex,
+    oldestIndex: oldestIndex,
+    content: content
+  };
+  _.extend(queue, queueMethods);
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() { return this.newestIndex - this.oldestIndex; },
+  enqueue: function(value) {
+    this.content[this.newestIndex] = value;
+    this.newestIndex++;
+  },
+  dequeue: function() {
+    var result;
+    if(this.size() > 0) {
+      result = this.content[this.oldestIndex];
+      delete this.content[this.oldestIndex];
+      this.oldestIndex++;
+    }
+    return result;
+  }
+};
 
 
