afbcfd3bebddba6757d9be1a787c1874088e5d44
apiga <ambroise.piganeau@gmail.com>
2015-10-10 14:15:53 -0700

    refactored sets

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,21 +1,39 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = {};
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  this._storage[item] = item;
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
-  return item in this._storage;
+  var result = false;
+  var i = 0;
+  while (!result && i < this._storage.length) {
+    if (this._storage[i] === item) {
+      result = true;
+    }
+    i++;
+  }
+  return result;
 };
 
 setPrototype.remove = function(item) {
-  delete this._storage[item];
+  var removed = false;
+  var i = 0;
+  while (!removed && i < this._storage.length) {
+    if (this._storage[i] === item) {
+      removed = true;
+      this._storage.splice(i, 1);
+    }
+    i++;
+  }
 };
 
 /*
